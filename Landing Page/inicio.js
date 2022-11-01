44e959e4e2900acdc341f9597759cf2ddcbe0163
Ismael Escalante <ismaelescalante18@gmail.com>
2022-11-01 13:06:23 +0100

    landing page

---
+++ Landing Page/inicio.js
@@ -0,0 +1,33 @@
+let modoRegistro = false;
+const a = document.querySelector('a');
+const h3 = document.querySelector('h3');
+const p = document.querySelector('p');
+const btn = document.querySelector('button')
+
+
+
+
+a.addEventListener('click', () => {
+    modoRegistro = !modoRegistro;
+    console.log(modoRegistro)
+
+    if(modoRegistro === true){
+        h3.innerText = 'Regístrate'
+        btn.innerText = 'Crear cuenta'
+        p.innerText = '¿Ya tienes cuenta?' 
+        a.innerText = 'Regístrate'
+    } 
+    
+    if(modoRegistro === false){
+        h3.innerText = 'Inicia sesión'
+        btn.innerText = 'Iniciar sesión'
+        p.innerText = '¿Ya tienes cuenta?'
+        a.innerText = 'Inicia sesión'
+    
+    }
+    
+   
+})
+
+
+
