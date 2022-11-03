92f4753e986652fd48327c863152ff2f42d68426
Ismael Escalante <ismaelescalante18@gmail.com>
2022-11-03 09:22:29 +0100

    cambiado el formulario

--- Landing Page/inicio.js
+++ Landing Page/inicio.js
@@ -1,33 +1,14 @@
-let modoRegistro = false;
-const a = document.querySelector('a');
-const h3 = document.querySelector('h3');
-const p = document.querySelector('p');
 const btn = document.querySelector('button')
+const email = document.querySelector('#email')
+const password = document.querySelector('#password')
 
 
 
 
-a.addEventListener('click', () => {
-    modoRegistro = !modoRegistro;
-    console.log(modoRegistro)
 
-    if(modoRegistro === true){
-        h3.innerText = 'Regístrate'
-        btn.innerText = 'Crear cuenta'
-        p.innerText = '¿Ya tienes cuenta?' 
-        a.innerText = 'Inicia sesión'
-    } 
-    
-    if(modoRegistro === false){
-        h3.innerText = 'Inicia sesión'
-        btn.innerText = 'Iniciar sesión'
-        p.innerText = '¿No tienes cuenta?'
-        a.innerText = 'Regístrate'
-    
-    }
-    
-   
-})
+
+
+
 
 
 
