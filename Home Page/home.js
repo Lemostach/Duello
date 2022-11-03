f55b49e76c0e1f7ace2a6e3cafa0c594b26280b4
Ismael Escalante <ismaelescalante18@gmail.com>
2022-11-03 13:43:49 +0100

    añadir lista

---
+++ Home Page/home.js
@@ -0,0 +1,7 @@
+const main = document.querySelector('main')
+const workspace = document.getElementsByClassName('workspace')
+const input = document.createElement('input')
+
+workspace.addEventListener('click', function(){
+    console.log('first')
+})
