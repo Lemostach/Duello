64cf15fc6aced63d4cc1670780da2af5b8e27655
Fulgencio <funche_em@hotmail.com>
2022-11-07 13:16:18 +0100

    cerrar sesión hecho

--- Home Page/home.js
+++ Home Page/home.js
@@ -1,9 +1,37 @@
-usuario;
+/* 
 const user = document.querySelector(".user")
-user.innerHTML.valueOf(usuario)
+Nombre del usuario */
+const userLog = JSON.parse(localStorage.getItem("usuarioLogeado"))
+   
+email = userLog[0].email
 
-document.getElementById("cerrar-sesion").onclick = function() {
-    localStorage.setItem("usuarioLogeado", "");
+/* cerrar sesión */
+
+const loggedUser = localStorage.getItem("usuarioLogeado");
+
+isLogged = (loggedUser) ? JSON.parse(loggedUser) : true;
+
+const exitBtn = document.querySelector('#cerrar-sesion')
+exitBtn.onclick = function() {
+
+    if(isLogged){
+        isLogged=!isLogged
+        localStorage.clear()
+       
+        location.assign("../Landing Page/inicio.html");
+        console.log("Hola")
+
+    }
+
+
+    
 }
+
+   
+
+
+
+    
+
 
 
