770870824c2998ba0b0d965980ea556400645bc5
Ismael Escalante <ismaelescalante18@gmail.com>
2022-11-07 09:54:30 +0100

    base de datos y js

--- Home Page/marcelo.js
+++ Home Page/marcelo.js
@@ -1,18 +1,48 @@
-const addList = document.querySelector(".addList")
-const form1 = document.querySelector(".form1")
-const form2 = document.querySelector(".form2")
-const form3 = document.querySelector(".form3")
-const form4 = document.querySelector(".form4")
-// const add = addList.addEventListener("click", )
-const input = document.querySelector(".nombreLista")
-const arrayForms = [form1, form2, form3, form4]
+const btn = document.querySelector('.addList')
+const input = document.querySelector('.nombreLista')
+const legend = document.querySelector('legend')
+const forms = document.querySelector('.forms')
+const visor = document.querySelector('.visor-tarea')
+
+const arrayDivs = [];
+const listas = [
+    {nombre: '', 
+    tareas: ''}
+]
 
 
-for (let index = 0; index < arrayForms.length; index++) {
-    addList.addEventListener("click", showform())
+btn.onclick = (e) => {
+    e.preventDefault()
+    let nuevaLista = {nombre: input.value, tareas: 'tarea'}
+    if(JSON.parse(localStorage.getItem('listas')) === null){
+        listas.shift()
+        listas.push(nuevaLista)
+    localStorage.setItem('listas', JSON.stringify(listas))
+    } else{
+        listas.push(nuevaLista)
+        localStorage.setItem('listas', JSON.stringify(listas))
+    }
     
+    
+    const list = document.createElement("div")
+    forms.appendChild(list)
+    list.className = 'list'
+    list.innerHTML = `<p>${input.value}</p> <div class='addTask'>Añadir tarea</div>`
+    input.value = ''
+
+    arrayDivs.push(list);
+    if(arrayDivs.length >= 4){
+        btn.disabled = true;
+    }
+
+    const addTask = document.querySelector('.addTask')
+
+addTask.addEventListener('click', () => {
+
+    visor.style.display = 'flex'
+})
 }
 
-function showform(form){
-    form.style.display = "grid"
-}
+
+
+
